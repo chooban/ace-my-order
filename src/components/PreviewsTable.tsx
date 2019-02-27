0c43d7ffff40e69f531b7129960eb0ef86a21bc6
Ross Hendry <rhendry@googlemail.com>
2019-02-27 18:05:51 +0000

    Responsive

--- src/components/PreviewsTable.tsx
+++ src/components/PreviewsTable.tsx
@@ -2,67 +2,78 @@
 
 import React, { PureComponent, useState } from 'react'
 import { withStyles, WithStyles, createStyles } from '@material-ui/core/styles'
+import { unstable_useMediaQuery as useMediaQuery } from '@material-ui/core/useMediaQuery'
+import Hidden from '@material-ui/core/Hidden';
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
+import { compose } from 'recompose'
 
 import { PreviewsItem } from "ace-my-order"
 
-const styles = createStyles({
-  root: {
-    width: '75vw',
-    marginTop: 3,
-    overflowX: 'auto',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-  },
-  table: {
-    minWidth: 700,
-  },
-})
-
-interface PreviewsTableProps extends WithStyles<typeof styles> {
-  rows: PreviewsItem[]
+const styles = (theme: any) => {
+  return createStyles({
+    root: {
+      [theme.breakpoints.down('sm')]: {
+        width: '100vw'
+      },
+      [theme.breakpoints.up('sm')]: {
+        width: '75vw'
+      },
+      marginTop: 3,
+      overflowX: 'auto',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+    cellTitle: {
+      [theme.breakpoints.down('md')]: {
+        textOverflow: 'ellipsis',
+        width: '30vw'
+      }
+    }
+  })
 }
 
-class PreviewsTable extends PureComponent<PreviewsTableProps, any> {
+function PreviewsTable(props: PreviewsTableProps) {
+  const { classes, rows } = props
 
-  render() {
-    const { classes, rows } = this.props
-
-    return (
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Description</TableCell>
-              <TableCell align="right">Price</TableCell>
+  return (
+    <Paper className={classes.root}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Description</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <Hidden smDown>
               <TableCell align="left">Publisher</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map(row => <Row key={row.code} row={row} />)}
-          </TableBody>
-        </Table>
-      </Paper>
-    )
-  }
+            </Hidden>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => <Row key={row.code} classes={classes} row={row} />)}
+        </TableBody>
+      </Table>
+    </Paper>
+  )
 }
 
-
-const Row = ({ row }: { row: PreviewsItem }) => {
+const Row = ({ row, classes }: RowProps) => {
 
   const [expanded, setExpanded] = useState(false)
 
   return (
     <React.Fragment>
       <TableRow onClick={() => setExpanded(!expanded)}>
-        <TableCell>{row.title}</TableCell>
+        <TableCell className={classes.cellTitle}>
+          {row.title}
+        </TableCell>
         <TableCell align="right">{row.price > 0 ? row.price: '\u2014' }</TableCell>
-        <TableCell>{row.publisher}</TableCell>
+        <Hidden smDown>
+          <TableCell>{row.publisher}</TableCell>
+        </Hidden>
       </TableRow>
       {expanded &&
         <TableRow>
@@ -75,4 +86,12 @@ const Row = ({ row }: { row: PreviewsItem }) => {
   )
 }
 
-export default withStyles(styles)(PreviewsTable)
+interface PreviewsTableProps extends WithStyles<typeof styles> {
+  rows: PreviewsItem[]
+}
+
+interface RowProps extends WithStyles<typeof styles> {
+  row: PreviewsItem
+}
+
+export default withStyles(styles, { withTheme: true })(PreviewsTable)
