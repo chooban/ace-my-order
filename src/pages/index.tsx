06654d6cd7a38316f631aad0f151be52b8256895
Ross Hendry <rhendry@googlemail.com>
2020-05-19 10:29:38 +0100

    Remove unused variables and code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,5 @@
-import { createStyles, withStyles } from '@material-ui/core/styles'
 import React from 'react'
 
-import { AllItemsIndexQuery } from '../../typings/autogen'
 import PreviewPanel from '../components/previews-table/PreviewPanel'
 
-const styles = (theme:any) => {
-  return createStyles({
-    root: {
-      width: '100%',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'start',
-      paddingLeft: '0.5em',
-      paddingTop: '14px',
-      overflow: 'hidden'
-    },
-  })
-}
-
-interface OwnProps {
-  classes: any,
-  data: AllItemsIndexQuery
-}
-
-const Index: React.FC<OwnProps> = ({ classes, data }) => {
-  return (
-    <PreviewPanel />
-  )
-}
-
-const styled = withStyles(styles, { withTheme: true })(Index)
-
-export default styled
+export default () => <PreviewPanel />
--- src/templates/item.tsx
+++ src/templates/item.tsx
@@ -1,38 +1,16 @@
-import { withStyles } from '@material-ui/core'
-import { createStyles } from '@material-ui/core/styles'
 import { graphql } from 'gatsby'
 import React from 'react'
 
 import { AceItem, ItemPageQuery } from '../../typings/autogen'
 import PreviewPanel from '../components/previews-table/PreviewPanel'
 
-const styles = (theme:any) => {
-  return createStyles({
-    root: {
-      width: '100%',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'start',
-      paddingLeft: '0.5em',
-      paddingTop: '14px',
-      overflow: 'hidden'
-    },
-  })
-}
-
 interface OwnProps {
-  classes: any,
   data: ItemPageQuery
 }
 
-const ItemPage: React.FC<OwnProps> = ({ classes, data }) => {
+const ItemPage: React.FC<OwnProps> = ({ data }) =>
+  (<PreviewPanel item={data.aceItem as AceItem} />)
 
-  return (
-    <PreviewPanel item={data.aceItem as AceItem} />
-  )
-}
 
 export const query = graphql`
   query ItemPage($previewsCode: String!) {
@@ -51,4 +29,4 @@ export const query = graphql`
   }
 `
 
-export default withStyles(styles, { withTheme: true })(ItemPage)
+export default ItemPage
